dbca0f781ab717787499fb1d36ef4b1a01a22a61
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-06-03 11:08:18 -0300

    initial setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const redis = require('redis')
+
+const server = express()
+const client = redis.createClient()
+const PORT = 3033
+
+client.set('visits', 0)
+
+server.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send('Numbe os visits: ' + visits)
+        client.set('visits', parseInt(visits) + 1)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listen on port ${PORT}`)
+})
